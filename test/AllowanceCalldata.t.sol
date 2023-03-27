687a94a4e318cd2b3b379df93ebb1663b877220d
Flydexo <flydexo@gmail.com>
2023-03-27 21:32:44 +0200

    forge install: openzeppelin-contracts
    v4.8.2

--- test/AllowanceCalldata.t.sol
+++ test/AllowanceCalldata.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/AllowanceCalldata.sol";
+import "../src/core/AllowanceCalldata.sol";
 
 contract AllowanceCalldataTest is Test {
     AllowanceCalldata public verifier;
